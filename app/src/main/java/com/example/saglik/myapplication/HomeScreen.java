7de0f00d0385cbe50d57448129721ba114e1ba06
FYB1905 <furkanyahyab@gmail.com>
2023-01-08 23:22:38 +0300

    Performans

--- app/src/main/java/com/example/saglik/myapplication/HomeScreen.java
+++ app/src/main/java/com/example/saglik/myapplication/HomeScreen.java
@@ -33,10 +33,7 @@ public class HomeScreen extends AppCompatActivity {
                 {
                     case R.id.home:
                         getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
-                        return true;
-                    case R.id.data:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.container, dataFragment).commit();
-                        return true;
+
                     case R.id.account:
                         getSupportFragmentManager().beginTransaction().replace(R.id.container, profileFragment).commit();
                         return true;
